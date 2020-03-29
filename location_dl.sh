ca5d4f2ce89ea9242c8aecc9e730a74312a6e8e1
Erica Xia <29347801+EricaXia@users.noreply.github.com>
2020-03-29 15:52:30 -0700

    update --count=5000 posts per loc id

--- location_dl.sh
+++ location_dl.sh
@@ -19,14 +19,14 @@ count=1
 for id in $array; do
 	count=$(( $count + 1 ))
 	if [ $(($count%4)) -eq 0 ]; then
-	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="sunwisdom101@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=10000 --request-timeout=300 $id	
+	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="sunwisdom101@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=5000 --request-timeout=300 $id	
 	elif [ $(($count%6)) -eq 0 ]; then	
-	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="musketeers1128@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=10000 --request-timeout=300 $id	
+	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="musketeers1128@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=5000 --request-timeout=300 $id	
 	elif [ $(($count%3)) -eq 0 ]; then	
-	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="jadeite.stream@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=10000 --request-timeout=300 $id	
+	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="jadeite.stream@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=5000 --request-timeout=300 $id	
 	elif [ $(($count%2)) -eq 0 ]; then	
-	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="katkit1112@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=10000 --request-timeout=300 $id	
+	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="katkit1112@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=5000 --request-timeout=300 $id	
 	else
-	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="cutebarfkitten@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=10000 --request-timeout=300 $id
+	python3 /home/rcf-proj3/aa6/ericaxia/instaloader-4.3/instaloader.py --login="cutebarfkitten@gmail.com" --password="EvOu7CR7Iq2V" --no-videos --geotags --comments --no-compress-json --post-filter="datetime(2020, 3, 1) <= date_utc <= datetime(2020, 3, 27)" --count=5000 --request-timeout=300 $id
 	fi
 done
